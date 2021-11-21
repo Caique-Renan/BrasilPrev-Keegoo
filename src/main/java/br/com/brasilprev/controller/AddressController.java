1f3da4ff712389bf925f49f4f4f69f77a5b1f093
Caique Renan <caique.renan21@gmail.com>
2021-11-20 21:13:55 -0300

    Implementando fluxo inicial do Endereco, sem testes.

--- src/main/java/br/com/brasilprev/controller/AddressController.java
+++ src/main/java/br/com/brasilprev/controller/AddressController.java
@@ -1,8 +1,29 @@
 package br.com.brasilprev.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.brasilprev.domain.AddressEntity;
+import br.com.brasilprev.service.AddressService;
+
+@RestController
+@RequestMapping("/address")
 public class AddressController {
+	
+	@Autowired
+	private AddressService addressService;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity <List<AddressEntity>> listar() {
+		return ResponseEntity.status(HttpStatus.OK).body(addressService.listar());
+	}
+	
+	
 
 }
--- src/main/java/br/com/brasilprev/service/AddressService.java
+++ src/main/java/br/com/brasilprev/service/AddressService.java
@@ -1,23 +1,21 @@
 package br.com.brasilprev.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.brasilprev.domain.AddressEntity;
+import br.com.brasilprev.repository.AddressRepository;
 
 @Service
 public class AddressService {
 
-	
 	@Autowired
-	private MailAddressService mailAddressService;
-	
-	
-	
-	public void teste() {
-		
-		mailAddressService.mailSend();
-		
+	private AddressRepository addressRepository;
+
+	public List<AddressEntity> listar() {
+		return addressRepository.findAll();
 	}
 
-	
-	
 }

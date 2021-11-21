680eaa4975531cbe0b56692a94dbdaeb39e6ea5a
Caique Renan <caique.renan21@gmail.com>
2021-11-20 22:08:17 -0300

    implementacao de Log para busca

--- src/main/java/br/com/brasilprev/service/AddressService.java
+++ src/main/java/br/com/brasilprev/service/AddressService.java
@@ -2,6 +2,8 @@ package br.com.brasilprev.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +12,17 @@ import br.com.brasilprev.repository.AddressRepository;
 
 @Service
 public class AddressService {
+	
+	public static final Logger log = LoggerFactory.getLogger(AddressService.class);
 
 	@Autowired
 	private AddressRepository addressRepository;
 
 	public List<AddressEntity> listar() {
-		return addressRepository.findAll();
+		
+		List<AddressEntity> addressList = addressRepository.findAll();
+		log.info("O numero de Enderecos e: {}", addressList.size());
+		return addressList;
 	}
 
 }
--- src/main/java/br/com/brasilprev/service/ClientService.java
+++ src/main/java/br/com/brasilprev/service/ClientService.java
@@ -2,6 +2,8 @@ package br.com.brasilprev.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +13,17 @@ import br.com.brasilprev.repository.ClientRepository;
 @Service
 public class ClientService {
 	
+	public static final Logger log = LoggerFactory.getLogger(ClientService.class);
+	
 	@Autowired
 	private ClientRepository clientRepository;
 
 	
 	public List<ClientEntity> listar() {
 
-		return clientRepository.findAll();
+		List<ClientEntity> clientList = clientRepository.findAll();
+		log.info("Numero de Clientes encontrados: {}", clientList.size());
+		return clientList;
 	}
 
 }

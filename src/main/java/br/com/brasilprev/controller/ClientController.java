75fe9a2c69682af60ca5ba1f7f1c2654c919b5a2
Caique Renan <caique.renan21@gmail.com>
2021-11-20 21:29:48 -0300

    Implementacao da funcao listar todos os clientes.

--- src/main/java/br/com/brasilprev/controller/ClientController.java
+++ src/main/java/br/com/brasilprev/controller/ClientController.java
@@ -1,8 +1,27 @@
 package br.com.brasilprev.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.brasilprev.domain.ClientEntity;
+import br.com.brasilprev.service.ClientService;
+
+@RestController
+@RequestMapping("/client")
 public class ClientController {
+	
+	@Autowired
+	private ClientService clientService;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity <List<ClientEntity>> listar() {
+		return ResponseEntity.status(HttpStatus.OK).body(clientService.listar());
+	}
 
 }
--- src/main/java/br/com/brasilprev/service/ClientService.java
+++ src/main/java/br/com/brasilprev/service/ClientService.java
@@ -1,8 +1,23 @@
 package br.com.brasilprev.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.brasilprev.domain.ClientEntity;
+import br.com.brasilprev.repository.ClientRepository;
 
 @Service
 public class ClientService {
+	
+	@Autowired
+	private ClientRepository clientRepository;
+
+	
+	public List<ClientEntity> listar() {
+
+		return clientRepository.findAll();
+	}
 
 }
